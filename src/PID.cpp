1812d52e875a6a83fff82b53b790f162db769e7d
Dario Cazzani <dcazzani@cisco.com>
2017-12-24 18:38:03 +0100

    First working solution

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,21 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  p_error = 0.0f;
+  i_error = 0.0f;
+  d_error = 0.0f;
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
-
